b7b30fc67fed3612797d0739a23bc18a3c8c10fd
CpCompany1017 <seretontour@gmail.com>
2022-11-07 18:55:37 +0200

    bundler setup

---
+++ src/bundler/index.ts
@@ -0,0 +1,38 @@
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkgPathPlugin";
+import { fetchPlugin } from "./plugins/fetchPlugin";
+
+let service: esbuild.Service;
+
+export const bundler = async (rawCode: string) => {
+  if (service) return;
+  service = await esbuild.startService({
+    worker: true,
+    wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+  });
+
+  try {
+    const result = await service.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+
+    return {
+      code: result.outputFiles[0].text,
+      error: "",
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        code: "",
+        error: error.message,
+      };
+    } else throw error;
+  }
+};
