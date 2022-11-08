52ce59f75921759a7980f845e2b73ff1f140612a
CpCompany1017 <seretontour@gmail.com>
2022-11-08 16:56:16 +0200

    setting monaco editor options

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -3,9 +3,19 @@ import MonacoEditor from "@monaco-editor/react";
 export const CodeEditor = () => {
   return (
     <MonacoEditor
-      height="90vh"
+      height="500px"
       language="javascript"
       theme="vs-dark"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
     />
   );
 };
