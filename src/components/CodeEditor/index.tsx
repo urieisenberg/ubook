32f55f1312c9a053ba2a1248cc21307d97e048e8
CpCompany1017 <seretontour@gmail.com>
2022-11-08 17:13:58 +0200

    adding a prettier to code editor n bulmaswatch css

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -1,4 +1,8 @@
+import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import styles from "./code-editor.module.css";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -9,7 +13,10 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   initialValue,
   onChange,
 }) => {
+  const editorRef = useRef<any>();
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
@@ -17,23 +24,40 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      value={initialValue}
-      editorDidMount={onEditorDidMount}
-      height="500px"
-      language="javascript"
-      theme="vs-dark"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <>
+      <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        height="500px"
+        language="javascript"
+        theme="vs-dark"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </>
   );
 };
