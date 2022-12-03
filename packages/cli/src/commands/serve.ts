13758528826b9780967ace3de722ceee68e7ee6a
urieisenberg <urieisenberg@gmail.com>
2022-12-03 16:28:00 +0200

    Wrapping express listen with a promise

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,12 +1,14 @@
-import path from 'path';
+import path from "path";
 import { Command } from "commander";
-import {serve} from 'local-api';
+import { serve } from "local-api";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options: {port :string}) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+    } catch (err) {}
   });
