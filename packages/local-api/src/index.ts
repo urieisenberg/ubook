c20d25e530c5ad328bab03ea9c35b99e60698657
urieisenberg <urieisenberg@gmail.com>
2022-12-04 13:17:55 +0200

    refactoring package names

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -22,7 +22,7 @@ export const serve = (
       })
     );
   } else {
-    const packagePath = require.resolve("local-client/build/index.html");
+    const packagePath = require.resolve("@ubook/local-client/build/index.html");
     app.use(express.static(path.dirname(packagePath)));
   }
 
